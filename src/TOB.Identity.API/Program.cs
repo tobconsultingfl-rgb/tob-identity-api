5d051e2d07ae6217b4504f146c32b5e53f6e902d
Thomas O'Brien <tobrien@reversevision.com>
2025-09-25 15:19:29 -0400

    Added Keyvault secret management

--- src/TOB.Identity.API/Program.cs
+++ src/TOB.Identity.API/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using System;
+
+
 namespace TOB.Identity.API;
 
 public class Program
@@ -12,6 +18,18 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var builtConfig = config.Build();
+                var keyVaultEndpoint = builtConfig["KeyVault:VaultUri"];
+
+                if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                {
+                    config.AddAzureKeyVault(
+                        new Uri(keyVaultEndpoint),
+                        new DefaultAzureCredential());
+                }
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseIISIntegration().UseStartup<Startup>();
